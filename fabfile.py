0e80114d4a1f742d54c6e6a34b2ee3c6d02378f9
ostrbor <ostrbor@gmail.com>
2017-06-17 14:13:37 +0300

    First commit

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+from fabric.api import local, task
+from fabric.context_managers import lcd
+
+BASE_DIR = 'src'
+
+
+@task
+def runserver():
+    with lcd(BASE_DIR):
+        local('./manage.py runserver')
+
+
+@task
+def migrate():
+    with lcd(BASE_DIR):
+        local('./manage.py makemigrations users')
+        local('./manage.py makemigrations accounts')
+        local('./manage.py migrate')
+        # local('./manage.py loaddata ../resources/fixtures/site.json')
+
+
+@task
+def createsuperuser():
+    with lcd(BASE_DIR):
+        local('./manage.py createsuperuser')
+
+
+@task
+def makemessages():
+    local(f'python {BASE_DIR}/manage.py makemessages')
+
+
+@task
+def compilemessages():
+    local(f'python {BASE_DIR}/manage.py compilemessages')
+
+
+@task
+def shell():
+    local(f'python {BASE_DIR}/manage.py shell_plus --ipython')
